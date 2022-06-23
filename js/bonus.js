ab03065a9bfa3f9dfebac3c6c20f5568b144cbb6
Soficius <103771506+Soficius@users.noreply.github.com>
2022-06-23 18:01:41 +0200

    divisione script in bonus

---
+++ js/bonus.js
@@ -0,0 +1,37 @@
+// 1. creo la costante container prendendo l'elemento con id container per stampare in pagina
+const container = document.getElementById('container');
+const row = document.createElement('div')
+row.className = 'row'
+container.append(row)
+// 2. creo il ciclo per stampare in console i numeri da 1 a 100 e per creare gli elementi da aggiungere al container.
+for (let i = 1; i<=100; i++){
+//  3. creo una nuovo elemento su JS
+    const newEl = document.createElement('div')
+
+//* aggiungo la classe flex al nuovo elemento creato
+    newEl.className = 'col'
+// 4. controllo se la variabile i sia multiplo sia di 3 che di 5
+    if(i % 3 == 0 && i % 5 == 0){
+
+// 5. valorizzo il nuovo elemento precedentemente dichiaro
+        newEl.innerText = 'FizzBuzz'
+        console.log("FizzBuzz");
+// 6. controllo se la variabile i è multiplo di 3
+    } else if(i % 3 == 0){
+// 7. valorizzo il nuovo elemento precedentemente dichiaro
+        newEl.innerText = 'Fizz'
+        console.log('Fizz');
+// 8. controllo se la varibiale i è multiplo di 5
+    } else if( i % 5 == 0){
+
+// 9. valorizzo il nuovo elemento precedentemente dichiaro
+        newEl.innerText = 'Buzz'
+        console.log('Buzz');
+// 10. se i controlli precedenti sono falsi valorizzo il nuovo elemento con la variabile i
+    } else {
+        newEl.innerText = i 
+        console.log(i);
+    }
+// 11.aggiungo il nuovo elemento al container
+    row.append(newEl)
+}
--- js/script.js
+++ js/script.js
@@ -1,34 +1,20 @@
-// 1. creo la costante container prendendo l'elemento con id container per stampare in pagina
-const container = document.getElementById('container');
-
-// 2. creo il ciclo per stampare in console i numeri da 1 a 100 e per creare gli elementi da aggiungere al container.
+// 1. creo il ciclo per stampare in console i numeri da 1 a 100
 for (let i = 1; i<=100; i++){
 
-//  3. creo una nuovo elemento su JS
-    const newEl = document.createElement('div')
-
-// 4. controllo se la variabile i sia multiplo sia di 3 che di 5
+// 2. controllo se la variabile i sia multiplo sia di 3 che di 5
     if(i % 3 == 0 && i % 5 == 0){
-
-// 5. valorizzo il nuovo elemento precedentemente dichiaro
-        newEl.innerText = 'FizzBuzz'
         console.log("FizzBuzz");
-// 6. controllo se la variabile i è multiplo di 3
+
+// 3. controllo se la variabile i è multiplo di 3
     } else if(i % 3 == 0){
-// 7. valorizzo il nuovo elemento precedentemente dichiaro
-        newEl.innerText = 'Fizz'
         console.log('Fizz');
+
 // 8. controllo se la varibiale i è multiplo di 5
     } else if( i % 5 == 0){
-
-// 9. valorizzo il nuovo elemento precedentemente dichiaro
-        newEl.innerText = 'Buzz'
         console.log('Buzz');
-// 10. se i controlli precedenti sono falsi valorizzo il nuovo elemento con la variabile i
+
+// 10. se i controlli precedenti sono falsi stampo i
     } else {
-        newEl.innerText = i 
         console.log(i);
     }
-// 11.aggiungo il nuovo elemento al container
-    container.append(newEl)
 }
